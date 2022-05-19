008df5e29c0d4e2ed91f0ad9cf445f03075453a0
Shahzod Akhmedov <shahzod.akhmedov@bk.ru>
2022-05-19 11:13:24 +0500

    Structuring try catches

--- Repository/Interfaces/FormRepository/FormRepository.cs
+++ Repository/Interfaces/FormRepository/FormRepository.cs
@@ -1,4 +1,6 @@
-﻿namespace Repository.Interfaces
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Repository.Interfaces
 {
     public class FormRepository : IFormRepository
     {
@@ -11,38 +13,42 @@
 
         public async Task<Response> InsertIntoDocument(DocumentDTO dto)
         {
-            FormToFill formApplication;
+            EntityEntry<Document> document = null;
+            EntityEntry<FormToFill> formApplication;
             try
             {
-                var document = await _context.documents.AddAsync(new Document
+                document = await _context.documents.AddAsync(new Document
                 {
                     DocumentNumber = dto.DocumentNumber,
                     cityId = dto.cityId,
                     DateOfIssue = dto.DateOfIssue,
                 });
                 await _context.SaveChangesAsync();
-                formApplication = new FormToFill
-                {
-                    documentId = document.Entity.Id
-                };
-                await _context.formsToFill.AddAsync(formApplication);
-                await _context.SaveChangesAsync();
             }
             catch (Exception ex)
             {
                 return new Response { Status = "400 || Error while adding into rote InsertIntoDocument!", Message = ex.InnerException.ToString() };
             }
-            return new Response { Status = "200", Message = $"OrderId => {formApplication.Id}" };
+            finally
+            {
+                formApplication = await _context.formsToFill.AddAsync(new FormToFill
+                {
+                    documentId = document.Entity.Id
+                });
+                await _context.SaveChangesAsync();
+            }
+            return new Response { Status = "200", Message = $"OrderId => {formApplication.Entity.Id}" };
         }
 
         public async Task<Response> InsertIntoPrincipalInfo(PrincipalInfoDTO dto)
         {
             var findOrder = await _context.formsToFill.FindAsync(dto.OrderId);
+            EntityEntry<PrincipalInfo> principalInfo = null;
             try
             {
                 if (findOrder == null)
                     return new Response { Status = "400", Message = $"order => {dto.OrderId} wasn't found!" };
-                var principalInfo = await _context.principalInfos.AddAsync(new PrincipalInfo
+                principalInfo = await _context.principalInfos.AddAsync(new PrincipalInfo
                 {
                     PrincipalPlaceId = dto.PrincipalPlaceId,
                     PrincipalPositionId = dto.PrincipalPositionId,
@@ -51,25 +57,28 @@
                     ReceiversAmount = dto.ReceiversAmount
                 });
                 await _context.SaveChangesAsync();
-                findOrder.principalInfoId = principalInfo.Entity.Id;
-                await _context.SaveChangesAsync();
             }
             catch (Exception ex)
             {
                 return new Response { Status = "400 || Error while adding into rote InsertIntoPrincipalInfo!", Message = ex.InnerException.ToString() };
             }
+            finally
+            {
+                findOrder.principalInfoId = principalInfo.Entity.Id;
+                await _context.SaveChangesAsync();
+            }
             return new Response { Status = "200", Message = "Success!" };
-            
         }
 
         public async Task<Response> InsertReceiversInfoId(ReceiversInfoDTO dto)
         {
             var findOrder = await _context.formsToFill.FindAsync(dto.OrderId);
+            EntityEntry<ReceiverInfo> receiverInfo = null;
             try
             {
                 if (findOrder == null)
                     return new Response { Status = "400", Message = $"order => {dto.OrderId} wasn't found!" };
-                var receiverInfo = await _context.receiverInfos.AddAsync(new ReceiverInfo
+                receiverInfo = await _context.receiverInfos.AddAsync(new ReceiverInfo
                 {
                     ReceiversFullname = dto.ReceiversFullname,
                     СitizenshipId = dto.СitizenshipId,
@@ -79,12 +88,15 @@
                     PassportDateOfIssue = dto.PassportDateOfIssue,
                 });
                 await _context.SaveChangesAsync();
-                findOrder.receiversInfoId = receiverInfo.Entity.Id;
-                await _context.SaveChangesAsync();
             }
             catch (Exception ex)
             {
                 return new Response { Status = "400 || Error while adding into rote InsertReceiversInfoId!", Message = ex.InnerException.ToString() };
+            }
+            finally
+            {
+                findOrder.receiversInfoId = receiverInfo.Entity.Id;
+                await _context.SaveChangesAsync();
             }
             return new Response { Status = "200", Message = "Success!" };
         }
